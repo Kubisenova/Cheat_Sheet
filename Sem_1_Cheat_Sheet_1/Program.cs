06ee3a1595b2f4798ad39d35800e2472961a5108
Aygul <Your>
2023-05-03 01:43:54 +0300

    Sem_1_Cheat_Sheet_1/

---
+++ Sem_1_Cheat_Sheet_1/Program.cs
@@ -0,0 +1,47 @@
+﻿// else
+// // если выражение не верное, то выполняется блок кода после else
+// {
+//     Console.WriteLine("x это НЕ пять");
+// }
+
+// // Цикл
+// int a = 0;
+// while (a < 5)
+// // если условие в скобках верное, то выполняется этот блок кода
+// {
+//     Console.WriteLine(a);
+//     a++; // увеличение a на единицу
+//     // равнозначно с:
+//     // a = a + 1;
+//     // a += 1;
+// }
+
+// форматирование текста в vs code - alt + shift + f (для Windows)
+
+// Вывод в консоль:
+// Console.WriteLine(переменная) - выводит переменную в консоль и переходит на новую строку
+// Console.Write(переменная) - выводит переменную в консоль и остаётся на этой строке
+
+// Console.WriteLine(1);
+// Console.WriteLine(2);
+
+// Console.Write(1);
+// Console.Write(234);
+
+// Чтение с консоли:
+// Console.ReadLine() - читает строку с консоли (до тех пор, пока не будет нажат enter)
+
+// string text = Console.ReadLine();
+
+// Перевод строки в число:
+
+// Convert.ToInt32(переменная) - переводит переменную в целое число (int)
+// Convert.ToDouble(переменная) - переводит переменную в дробное число число (double)
+
+// Console.WriteLine("Введите число: ");
+// string text = Console.ReadLine();
+// int number = Convert.ToInt32(text);
+
+// int abc = number * number;
+// Console.WriteLine("Ответ: ");
+// Console.WriteLine(abc);
